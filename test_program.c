47c49d3a49f00293cc816fa998b45c60a270dabd
Sterling De Jesus <s.dejesusp@gmail.com>
2020-02-17 00:55:10 -0400

    Free memory resources

--- test_program.c
+++ test_program.c
@@ -13,15 +13,16 @@ int main() {
     // Example 1: 2.56 x 5  
     result = multiplyStrNumberByInteger(5, "2.56");
     printf("The result is: %s\n", result);
+    free(result);
 
     // Example 2: 100.99 x 45
     result = multiplyStrNumberByInteger(45, "100.99");
     printf("The result is: %s\n", result);
+    free(result);
 
     // Example 3: 489.87 x 23
     result = multiplyStrNumberByInteger(23, "489.87");
-    printf("The result is: %s\n", result);
-     
+    printf("The result is: %s\n", result); 
     free(result); 
 
     return 0;
